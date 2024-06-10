538bbc8bbfb28bd8c9bbbe4cb26fb9c467d09fbb
Zhennor <139512238+Zhennor@users.noreply.github.com>
2024-06-10 17:10:35 +0700

    Delete data directory

--- data/tilesets/Tileset SnowyWorld by Thurpok on DeviantArt.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Tileset SnowyWorld by Thurpok on DeviantArt" tilewidth="64" tileheight="64" tilecount="24" columns="4">
- <image source="G:/data/graphics/tilesets/Tileset SnowyWorld by Thurpok on DeviantArt.jpeg" width="256" height="430"/>
-</tileset>
--- data/tilesets/snow.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="R" tilewidth="64" tileheight="64" tilecount="192" columns="16">
- <image source="../graphics/tilesets/Snow.png" width="1024" height="803"/>
-</tileset>
